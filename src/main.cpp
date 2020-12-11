aa9daba55b0fafc08c315d26b57860255035060d
protobit <admin@protobyte.tech>
2020-12-11 17:49:58 +0000

    Added main file

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,13 @@
 // Created by protobit on 11/12/2020.
 //
 
+/**
+ * Main function
+ * @param argc
+ * @param argv
+ * @return process ret value
+ */
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
